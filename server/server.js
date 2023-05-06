1276e560dc413484adccc2906b6d0f34e47be59d
Aman Kumar Gupta <75511608+codecraft26@users.noreply.github.com>
2023-05-07 00:32:32 +0530

    deleting frontend part some testing

--- server/server.js
+++ server/server.js
@@ -1,17 +1,14 @@
 const port = 4000
 const express = require('express')
 const app = express()
-const path = require("path");
 
-
+app.get('/', (req, res) => {
+    res.send('<h1> Hello World </h1>')
+    }
+)
 app.listen(port, () => {
 
     console.log(`Server is running on port ${port}`)
 }
 )
-app.use(express.static(path.join(__dirname, "../client/build")));
 
-app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client/build/static/index.html"));
-  });
-  
